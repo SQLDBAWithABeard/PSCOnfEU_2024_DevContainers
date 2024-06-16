25027ac5c655510a4aa07267cf01badf75d63198
Rob Sewell <mrrobsewell@outlook.com>
2024-06-16 11:03:37 +0100

    so that we can tab complete

--- newfile.ps1
+++ newfile.ps1
@@ -1,11 +1,46 @@
-# Import the required module
-Import-Module -Name Microsoft.PowerShell.Utility
+using namespace System.Management.Automation
+using namespace System.Collections.Generic
 
-# Define the URL of the UK Velo website
-$websiteUrl = "https://www.ukvelo.co.uk"
+class EventNameCompletion : System.Management.Automation.IValidateSetValuesGenerator {
+    [string[]] GetValidValues() {
 
-# Send a web request to the website and retrieve the results
-$response = Invoke-WebRequest -Uri $websiteUrl
+        # Define the URL of the results page
+        $resultsPageUrl = 'https://www.ukvelo.co.uk/results.html'
 
-# Display the response content
-$response.Content
+        # Use Invoke-WebRequest to fetch the content of the page
+        $response = Invoke-WebRequest -Uri $resultsPageUrl
+        $what = $response.Content -split '<div class="col-sm-4 post-snippet masonry-item">'
+        $what = $what[1..($what.Length - 1)]
+
+        # Use a regex pattern to match the content inside the <h5> tag
+        $Namepattern = '<h5 class="mb0">(?<value>.+?)</h5>'
+        $Linkpattern = '<a class="btn btn-sm btn-rounded btn-filled" href="(?<value>.+?)" target="_blank">GET RESULTS</a>'
+
+        $eventNames = foreach ($item in $what) {
+            [PSCustomObject]@{
+                Name      = [regex]::Match($item, $Namepattern).Groups['value'].Value
+                EventLink = [regex]::Match($item, $Linkpattern).Groups['value'].Value
+            }
+        }
+
+
+        # Return the list of event names
+        return [string[]] $eventNames.Name
+    }
+}
+
+function Get-EventDetails {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Enter the name of the event.")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet([EventNameCompletion])]
+        [string]$EventName
+    )
+
+    # Rest of the function code...
+}
+
+# Example usage:
+# $eventDetails = Get-EventDetails -EventName "SOMERSET SPORTIVE"
+# $eventDetails | Format-Table -AutoSize
